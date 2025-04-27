88c993d4d1db90605ba5a2585a9ce61623573766
itsairanggg <airamaecorpuz86@gmail.com>
2025-04-27 08:52:16 +0800

    Take Home Exercises on PHP+MySql (CMPSC 117 - CORPUZ)

---
+++ dbcontacts.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2025 at 02:50 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcontacts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsms`
+--
+
+CREATE TABLE `tblsms` (
+  `sms_ID` int(11) NOT NULL,
+  `studno` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `cpno` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblsms`
+--
+ALTER TABLE `tblsms`
+  ADD PRIMARY KEY (`sms_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblsms`
+--
+ALTER TABLE `tblsms`
+  MODIFY `sms_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
